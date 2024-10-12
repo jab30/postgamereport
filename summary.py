e20255fe14378033756c218986a94700b88bb84d
jab30 <jacksonabernathy13@gmail.com>
2024-10-11 20:33:57 -0400

    Update summary.py

--- summary.py
+++ summary.py
@@ -50,7 +50,7 @@ def load_data():
     data_files = [
         '20241004-KennesawWalterKelly-Private-1_unverified.csv',
         '20241005-KennesawWalterKelly-Private-2_unverified.csv',
-        '20241010-KennesawWalterKelly-Private-2_unverified.csv',
+        '20241010-KennesawWalterKelly-Private-1_unverified.csv',
         '20241011-KennesawWalterKelly-Private-2_unverified.csv'
     ]
 
