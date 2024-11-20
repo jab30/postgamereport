78e1c1cc9c55c0f9275cff45bf07604c1ff6b2a3
jab30 <jacksonabernathy13@gmail.com>
2024-11-20 10:07:10 -0500

    Update summary.py

--- summary.py
+++ summary.py
@@ -55,7 +55,7 @@ def load_data():
         '20241101-KennesawWalterKelly-Private-1_unverified.csv',
         '20241031-KennesawWalterKelly-Private-1_unverified.csv',
         '20241018-GeorgiaTech-Private-3_unverified.csv',
-        '20241118-KennesawWalterKelly-Private-1_unverified.csv'
+        '20241118-KennesawWalterKelly-Private-2_unverified.csv'
     ]
 
     all_data = pd.DataFrame()
