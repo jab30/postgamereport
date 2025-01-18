15cd4094baf397ca4930cf555cbe58192e5f4b09
jab30 <jacksonabernathy13@gmail.com>
2025-01-18 17:32:12 -0500

    Update summary.py

--- summary.py
+++ summary.py
@@ -42,6 +42,16 @@ def add_strike_zone(ax):
 @st.cache_data
 def load_data():
     data_files = [
+        '20241004-KennesawWalterKelly-Private-1_unverified.csv',
+        '20241005-KennesawWalterKelly-Private-2_unverified.csv',
+        '20241010-KennesawWalterKelly-Private-1_unverified.csv',
+        '20241011-KennesawWalterKelly-Private-2_unverified.csv',
+        '20241101-KennesawWalterKelly-Private-1_unverified.csv',
+        '20241031-KennesawWalterKelly-Private-1_unverified.csv',
+        '20241018-GeorgiaTech-Private-3_unverified.csv',
+        '20241118-KennesawWalterKelly-Private-2_unverified.csv',
+        '20241121-KennesawWalterKelly-Private-4_unverified.csv',
+        '20250109-KennesawWalterKelly-Private-1_unverified.csv',
         '20250116-KennesawWalterKelly-Private-1_unverified.csv',
         '20250117-KennesawWalterKelly-Private-1_unverified.csv'
     ]
