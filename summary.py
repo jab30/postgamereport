28f0addb9fe63526dcbdb703716a5d45c9eaa8e2
jab30 <jacksonabernathy13@gmail.com>
2025-02-19 21:36:30 -0500

    Update summary.py

--- summary.py
+++ summary.py
@@ -58,7 +58,8 @@ def load_data():
         '20250130-KennesawWalterKelly-Private-1_unverified.csv',
         '20250214-KennesawWalterKelly-1.csv',
         '20250214-KennesawWalterKelly-2.csv',
-        '20250218-KennesawWalterKelly-1_unverified.csv'
+        '20250218-KennesawWalterKelly-1_unverified.csv',
+        '20250219-KennesawWalterKelly-1_unverified.csv'
     ]
 
     all_data = pd.DataFrame()
